32c331f1efe876e89de3693f08544e8cb7c19074
mattsuDev <dynamic.dramatic.developer@gmail.com>
2015-06-21 17:08:37 +0900

    画像の読み込み処理を追加

---
+++ ImageSample/ImagePage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace ImageSample
+{
+    public class ImagePage : ContentPage
+    {
+        public ImagePage()
+        {
+            // Imageビューの生成
+            var image = new Image
+            {
+                // 画像を読み込んでSourceプロパティに設定
+                Source = ImageSource.FromResource("ImageSample.Resources.Images.button_leftArrow.png"),
+            };
+            
+            Content = new StackLayout
+            { 
+                // 画面中央からレイアウト
+                VerticalOptions = LayoutOptions.Center,
+                Children =
+                {
+                    image,
+                }
+            };
+        }
+    }
+}
+
+
--- ImageSample/ImageSample.cs
+++ ImageSample/ImageSample.cs
@@ -8,22 +8,8 @@ namespace ImageSample
     {
         public App()
         {
-            // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children =
-                    {
-                        new Label
-                        {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new ImagePage();
+            return;
         }
 
         protected override void OnStart()
